f927e0cf85437db28786e361a785b36c73cec702
AzeddineZaraa <82954128+AzeddineZaraa@users.noreply.github.com>
2026-04-23 23:50:31 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+st.set_page_config(page_title="Digital Twin PV", layout="wide")
+
+st.title("⚡ Digital Twin - Installation Photovoltaïque")
+
+st.write("Bienvenue dans votre dashboard en ligne 🚀")
+
+st.metric("Puissance (W)", 2500)
+st.metric("Tension (V)", 230)
+st.metric("Courant (A)", 10)
